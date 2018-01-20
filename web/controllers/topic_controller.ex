4d0ffb1ac5ca66161cf9dfca3e38246242e48906
dmcbrayer <dmcbrayer@gmail.com>
2018-01-20 13:11:59 -0500

    Adds a plug to implement full sign in and sign out flow.

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -55,7 +55,7 @@ defmodule Discuss.TopicController do
     Repo.get!(Topic, topic_id) |> Repo.delete!
 
     conn 
-      |> put_flash(:info, "Topic Deleted") 
-      |> redirect(to: topic_path(conn, :index))
+    |> put_flash(:info, "Topic Deleted") 
+    |> redirect(to: topic_path(conn, :index))
   end
 end
