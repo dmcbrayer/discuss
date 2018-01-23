e893ebbf39d269189a0d36203bf64f9d0461721a
dmcbrayer <dmcbrayer@gmail.com>
2018-01-22 21:10:47 -0500

    Adds some websockets stuff.

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -2,7 +2,7 @@ defmodule Discuss.Comment do
   use Discuss.Web, :model
 
   schema "comments" do
-    field :content, :text
+    field :content, :string
     belongs_to :user, Discuss.User
     belongs_to :topic, Discuss.Topic
 
